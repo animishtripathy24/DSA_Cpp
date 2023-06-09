a1d75353e055e546d9c0f13527ad894865ff7948
Animish Tripathy <135792519+animishtripathy24@users.noreply.github.com>
2023-06-09 13:05:16 +0530

    Merge Two Sorted Lists_in O(1) S.C

---
+++ Merge Two Sorted Lists_in O(1) S.C
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+ 
+ 
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        //if the both is empty in this case we return null
+        if(list1==NULL && list2==NULL)
+        {
+            return NULL;
+        }
+        //if the first list is empty in this case we return second list
+        if(list1==NULL)
+        {
+            return list2;
+        }
+        //if the second list is empty in this case we return first list
+        if(list2==NULL)
+        {
+            return list1;
+        }
+
+        //mark the list1 pointer pointer to the smallest node at first
+        if(list1->val > list2->val)
+        {
+            swap(list1,list2);
+        }
+        ListNode* res=list1;
+
+        while(list1 != NULL && list2 != NULL)
+        {
+            //make a temporary pointer
+            ListNode* temp=NULL;
+            while(list1 != NULL &&  list1->val <= list2->val)
+            {
+                temp=list1;
+                list1=list1->next;
+            }
+            temp->next=list2;
+            swap(list1,list2);
+        }
+
+        return res;
+    }
+};
