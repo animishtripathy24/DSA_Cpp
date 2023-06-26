789468bdaf9904563f8c0e84e8a2cef7aed837ed
Animish Tripathy <135792519+animishtripathy24@users.noreply.github.com>
2023-06-26 11:45:29 +0530

    637. Average of Levels in Binary Tree
    Given the root of a binary tree, return the average value of the nodes on each level in the form of an array. Answers within 10-5 of the actual answer will be accepted.
     
    
    Example 1:
    
    
    Input: root = [3,9,20,null,null,15,7]
    Output: [3.00000,14.50000,11.00000]
    Explanation: The average value of nodes on level 0 is 3, on level 1 is 14.5, and on level 2 is 11.
    Hence return [3, 14.5, 11].
    Example 2:
    
    
    Input: root = [3,9,20,15,7]
    Output: [3.00000,14.50000,11.00000]

---
+++ 637. Average of Levels in Binary Tree solution.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<double> averageOfLevels(TreeNode* root) {
+        if(root==NULL)
+        {
+            return {};
+        }
+        vector<double>v;
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty())
+        {
+            long long int sum=0;
+            int size=q.size();
+            for(int i=0;i<size;i++)
+            {
+                TreeNode* curr=q.front();
+                q.pop();
+                if(curr->left)
+                {
+                    q.push(curr->left);
+                }
+                if(curr->right)
+                {
+                    q.push(curr->right);
+                }
+                sum+=curr->val;
+
+            }
+
+            v.push_back((sum/(double)size));
+        }
+
+        return v;
+        
+    }
+};
