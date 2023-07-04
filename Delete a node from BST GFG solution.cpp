1d7ece28d90dfacd9346f93c37abe7d4fd9e1542
Animish Tripathy <135792519+animishtripathy24@users.noreply.github.com>
2023-07-04 12:09:44 +0530

    Delete a node from BST GFG
    Given a Binary Search Tree and a node value X. Delete the node with the given value X from the BST. If no node with value x exists, then do not make any change. 
    
    Example 1:
    
    Input:
          2
        /   \
       1     3
    X = 12
    Output: 1 2 3
    Explanation: In the given input there
    is no node with value 12 , so the tree
    will remain same.
    Example 2:
    
    Input:
                1
                 \
                  2
                    \
                     8 
                   /    \
                  5      11
                /  \    /  \
               4    7  9   12
    X = 9
    Output: 1 2 4 5 7 8 11 12
    Explanation: In the given input tree after
    deleting 9 will be
                 1
                  \
                   2
                     \
                      8
                     /   \
                    5     11
                   /  \     \
                  4    7     12
    Your Task:
    You don't need to read input or print anything. Your task is to complete the function deleteNode() which takes two arguments. The first being the root of the tree, and an integer 'X' denoting the node value to be deleted from the BST. Return the root of the BST after deleting the node with value X. Do not make any update if there's no node with value X present in the BST.
    
    Note: The generated output will be the inorder traversal of the modified tree.
     
    
    Expected Time Complexity: O(Height of the BST).
    Expected Auxiliary Space: O(Height of the BST).
     
    
    Constraints:
    1 <= N <= 105

---
+++ Delete a node from BST GFG solution.cpp
@@ -0,0 +1,64 @@
+Node* findLastRightChildOnLeft(Node* root)
+{
+    if(root->right ==NULL)
+    {
+        return root;
+    }
+    return findLastRightChildOnLeft(root->right);
+}
+Node* helper(Node* root)
+{
+    if(root->left==NULL)
+    {
+        return root->right;
+    }
+    if(root->right==NULL)
+    {
+        return root->right;
+    }
+    Node* rightChild=root->right;
+    Node* lastRight=findLastRightChildOnLeft(root->left);
+    lastRight->right=rightChild;
+    return root->left;
+}
+// Function to delete a node from BST.
+Node *deleteNode(Node *root, int X) {
+    if(root==NULL)
+    {
+        return NULL;
+    }
+    if(root->data == X)
+    {
+        return helper(root);
+    }
+    
+    Node* dummy=root;
+    while(root)
+    {
+        if(root->data > X) //it means that key must lie on the left subtree
+        {
+            if(root->left != NULL && root->left->data==X)
+            {
+                root->left=helper(root->left);
+                break;
+            }
+            else
+            {
+                root=root->left;
+            }
+        }
+        else
+        {
+            if(root->right != NULL && root->right->data==X)
+            {
+                root->right=helper(root->right);
+                break;
+            }
+            else
+            {
+                root=root->right;
+            }
+        }
+    }
+    return dummy;
+}
