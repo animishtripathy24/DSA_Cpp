e12f376243438daa8d53328ebcbc2010b91064ef
Animish Tripathy <135792519+animishtripathy24@users.noreply.github.com>
2024-04-14 18:04:36 +0530

    Xoring and Clearing gfg sol.cpp

---
+++ Xoring and Clearing gfg sol.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    void printArr(int n, int arr[]) {
+        for(int i=0;i<n;i++)
+        {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+    }
+
+    void setToZero(int n, int arr[]) {
+        for(int i=0;i<n;i++)
+        {
+            arr[i]*=0;
+        }
+    }
+
+    void xor1ToN(int n, int arr[]) {
+        // Xor arr[i]^i]
+        for(int i=0;i<n;i++)
+        {
+            arr[i]^=i;
+        }
+    }
+};
