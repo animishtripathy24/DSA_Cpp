f30f034c2de40b4bb0af3e3a6c3c083e67497cb1
Animish Tripathy <135792519+animishtripathy24@users.noreply.github.com>
2024-07-31 12:02:17 +0530

    Power Set(find all the subsets of the given array)sol.cpp

---
+++ Power Set(find all the subsets of the given array)sol.cpp
@@ -0,0 +1,18 @@
+vector<vector<int>> getPowerSet(vector<int> &nums) {
+    vector<vector<int>>ans;
+	int n = nums.size();
+	int subsets = 1 << n;
+	for(int i=0;i<subsets;i++)
+	{
+		vector<int>temp;
+		for(int j=0;j<n;j++)
+		{
+			if(i & (1<<j))
+			{
+				temp.push_back(nums[j]);
+			}
+		}
+		ans.push_back(temp);
+	}
+	return ans;
+}
